e8d0338bed21ff157317a22bd6914e8eeac2ed1c
Kyoengpil Lee <kplee@cern.ch>
2017-09-04 21:01:16 +0900

    QCD samples

---
+++ DYntupleMaker/ntuples/CRABSubmit/crab3cfg_MC_QCDPtBinned_Moriond17.py
@@ -0,0 +1,143 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.General.requestName = ''
+config.General.workArea = 'DYntuple'
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = '../MC_cfg_80X_mcRun2_asymptotic_2016_TrancheIV_v6.py'
+
+config.Data.inputDataset = ''
+
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 2
+config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
+config.Data.publication = False
+# config.JobType.maxJobRuntimeMin = 2700 # -- 36 hours -- #
+
+config.Site.storageSite = 'T2_KR_KNU'
+
+version = '_v20170904_'
+# 'MultiCRAB' part
+if __name__ == '__main__':
+    
+    from CRABAPI.RawCommand import crabCommand
+
+    config.General.requestName = 'DYntuple'+version+'QCD_Pt20toInf'
+    config.Data.inputDataset = '/QCD_Pt-20toInf_MuEnrichedPt15_TuneCUETP8M1_13TeV_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
+    crabCommand('submit', config = config)
+
+
+    config.General.requestName = 'DYntuple'+version+'QCD_Pt15to20'
+    config.Data.inputDataset = '/QCD_Pt-15to20_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
+    crabCommand('submit', config = config)
+
+    config.General.requestName = 'DYntuple'+version+'QCD_Pt20to30'
+    config.Data.inputDataset = '/QCD_Pt-20to30_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
+    crabCommand('submit', config = config)
+
+
+    config.General.requestName = 'DYntuple'+version+'QCD_Pt30to50'
+    config.Data.inputDataset = '/QCD_Pt-30to50_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
+    crabCommand('submit', config = config)
+
+    config.General.requestName = 'DYntuple'+version+'QCD_Pt50to80'
+    config.Data.inputDataset = '/QCD_Pt-50to80_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
+    crabCommand('submit', config = config)
+
+
+    config.General.requestName = 'DYntuple'+version+'QCD_Pt80to120'
+    config.Data.inputDataset = '/QCD_Pt-80to120_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
+    crabCommand('submit', config = config)
+
+    config.General.requestName = 'DYntuple'+version+'QCD_Pt80to120_ext1'
+    config.Data.inputDataset = '/QCD_Pt-80to120_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v3/MINIAODSIM'
+    crabCommand('submit', config = config)
+
+
+    config.General.requestName = 'DYntuple'+version+'QCD_Pt120to170'
+    config.Data.inputDataset = '/QCD_Pt-120to170_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
+    crabCommand('submit', config = config)
+
+    config.General.requestName = 'DYntuple'+version+'QCD_Pt120to170_backup'
+    config.Data.inputDataset = '/QCD_Pt-120to170_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISummer16MiniAODv2-PUMoriond17_backup_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
+    crabCommand('submit', config = config)
+
+
+    config.General.requestName = 'DYntuple'+version+'QCD_Pt170to300'
+    config.Data.inputDataset = '/QCD_Pt-170to300_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
+    crabCommand('submit', config = config)
+
+    config.General.requestName = 'DYntuple'+version+'QCD_Pt170to300_ext1'
+    config.Data.inputDataset = '/QCD_Pt-170to300_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/MINIAODSIM'
+    crabCommand('submit', config = config)
+
+    config.General.requestName = 'DYntuple'+version+'QCD_Pt170to300_backup'
+    config.Data.inputDataset = '/QCD_Pt-170to300_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISummer16MiniAODv2-PUMoriond17_backup_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
+    crabCommand('submit', config = config)
+
+
+    # config.General.requestName = 'DYntuple'+version+'QCD_Pt300to470'
+    # config.Data.inputDataset = '/QCD_Pt-300to470_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
+    # crabCommand('submit', config = config)
+
+    # config.General.requestName = 'DYntuple'+version+'QCD_Pt300to470_ext1'
+    # config.Data.inputDataset = '/QCD_Pt-300to470_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/MINIAODSIM'
+    # crabCommand('submit', config = config)
+
+    # config.General.requestName = 'DYntuple'+version+'QCD_Pt300to470_ext2'
+    # config.Data.inputDataset = '/QCD_Pt-300to470_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext2-v1/MINIAODSIM'
+    # crabCommand('submit', config = config)
+
+
+    # config.General.requestName = 'DYntuple'+version+'QCD_Pt470to600'
+    # config.Data.inputDataset = '/QCD_Pt-470to600_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
+    # crabCommand('submit', config = config)
+
+    # config.General.requestName = 'DYntuple'+version+'QCD_Pt470to600_ext1'
+    # config.Data.inputDataset = '/QCD_Pt-470to600_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/MINIAODSIM'
+    # crabCommand('submit', config = config)
+
+    # config.General.requestName = 'DYntuple'+version+'QCD_Pt470to600_ext2'
+    # config.Data.inputDataset = '/QCD_Pt-470to600_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext2-v1/MINIAODSIM'
+    # crabCommand('submit', config = config)
+
+
+    # config.General.requestName = 'DYntuple'+version+'QCD_Pt600to800'
+    # config.Data.inputDataset = '/QCD_Pt-600to800_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
+    # crabCommand('submit', config = config)
+
+    # config.General.requestName = 'DYntuple'+version+'QCD_Pt600to800_ext1'
+    # config.Data.inputDataset = '/QCD_Pt-600to800_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/MINIAODSIM'
+    # crabCommand('submit', config = config)
+
+    # config.General.requestName = 'DYntuple'+version+'QCD_Pt600to800_backup'
+    # config.Data.inputDataset = '/QCD_Pt-600to800_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISummer16MiniAODv2-PUMoriond17_backup_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
+    # crabCommand('submit', config = config)
+
+
+    # config.General.requestName = 'DYntuple'+version+'QCD_Pt800to1000'
+    # config.Data.inputDataset = '/QCD_Pt-800to1000_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
+    # crabCommand('submit', config = config)
+
+    # config.General.requestName = 'DYntuple'+version+'QCD_Pt800to1000_ext1'
+    # config.Data.inputDataset = '/QCD_Pt-800to1000_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/MINIAODSIM'
+    # crabCommand('submit', config = config)
+
+    # config.General.requestName = 'DYntuple'+version+'QCD_Pt800to1000_ext2'
+    # config.Data.inputDataset = '/QCD_Pt-800to1000_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext2-v1/MINIAODSIM'
+    # crabCommand('submit', config = config)
+
+
+    # config.General.requestName = 'DYntuple'+version+'QCD_Pt1000toInf'
+    # config.Data.inputDataset = '/QCD_Pt-1000toInf_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
+    # crabCommand('submit', config = config)
+
+    # config.General.requestName = 'DYntuple'+version+'QCD_Pt1000toInf_ext1'
+    # config.Data.inputDataset = '/QCD_Pt-1000toInf_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v3/MINIAODSIM'
+    # crabCommand('submit', config = config)
+
+    # config.General.requestName = ''
+    # config.Data.inputDataset = ''
+    # crabCommand('submit', config = config)
