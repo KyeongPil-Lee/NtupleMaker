11f7f41e4cdf928f10caa16d6948a2e2d72f60cb
Kyeongpil Lee <kplee@cern.ch>
2017-10-23 17:38:19 +0900

    update MC lists

--- DYntupleMaker/ntuples/CRABSubmit/crab3cfg_MC_BackgroundMC_Moriond17.py
+++ DYntupleMaker/ntuples/CRABSubmit/crab3cfg_MC_BackgroundMC_Moriond17.py
@@ -24,33 +24,33 @@ if __name__ == '__main__':
     
     from CRABAPI.RawCommand import crabCommand
 
-    # config.General.requestName = 'DYntuple'+version+'ttbarTo2L2Nu'
-    # config.Data.inputDataset = '/TTTo2L2Nu_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
-    # crabCommand('submit', config = config)
+    config.General.requestName = 'DYntuple'+version+'ttbarTo2L2Nu'
+    config.Data.inputDataset = '/TTTo2L2Nu_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
+    crabCommand('submit', config = config)
 
-    # config.General.requestName = 'DYntuple'+version+'ttbar'
-    # config.Data.inputDataset = '/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
-    # crabCommand('submit', config = config)
+    config.General.requestName = 'DYntuple'+version+'ttbar'
+    config.Data.inputDataset = '/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
+    crabCommand('submit', config = config)
 
-    # config.General.requestName = 'DYntuple'+version+'ttbarBackup'
-    # config.Data.inputDataset = '/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8/RunIISummer16MiniAODv2-PUMoriond17_backup_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
-    # crabCommand('submit', config = config)
+    config.General.requestName = 'DYntuple'+version+'ttbarBackup'
+    config.Data.inputDataset = '/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8/RunIISummer16MiniAODv2-PUMoriond17_backup_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
+    crabCommand('submit', config = config)
 
-    # config.General.requestName = 'DYntuple'+version+'WW'
-    # config.Data.inputDataset = '/WW_TuneCUETP8M1_13TeV-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/MINIAODSIM'
-    # crabCommand('submit', config = config)
+    config.General.requestName = 'DYntuple'+version+'WW'
+    config.Data.inputDataset = '/WW_TuneCUETP8M1_13TeV-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/MINIAODSIM'
+    crabCommand('submit', config = config)
 
-    # config.General.requestName = 'DYntuple'+version+'WWTo2L2Nu'
-    # config.Data.inputDataset = '/WWTo2L2Nu_13TeV-powheg/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
-    # crabCommand('submit', config = config)
+    config.General.requestName = 'DYntuple'+version+'WWTo2L2Nu'
+    config.Data.inputDataset = '/WWTo2L2Nu_13TeV-powheg/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
+    crabCommand('submit', config = config)
 
-    # config.General.requestName = 'DYntuple'+version+'WZ'
-    # config.Data.inputDataset = '/WZ_TuneCUETP8M1_13TeV-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/MINIAODSIM'
-    # crabCommand('submit', config = config)
+    config.General.requestName = 'DYntuple'+version+'WZ'
+    config.Data.inputDataset = '/WZ_TuneCUETP8M1_13TeV-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/MINIAODSIM'
+    crabCommand('submit', config = config)
 
-    # config.General.requestName = 'DYntuple'+version+'WZTo3LNu'
-    # config.Data.inputDataset = '/WZTo3LNu_TuneCUETP8M1_13TeV-powheg-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
-    # crabCommand('submit', config = config)
+    config.General.requestName = 'DYntuple'+version+'WZTo3LNu'
+    config.Data.inputDataset = '/WZTo3LNu_TuneCUETP8M1_13TeV-powheg-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v3/MINIAODSIM'
+    crabCommand('submit', config = config)
 
     config.General.requestName = 'DYntuple'+version+'ZZ'
     config.Data.inputDataset = '/ZZ_TuneCUETP8M1_13TeV-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/MINIAODSIM'
@@ -60,13 +60,17 @@ if __name__ == '__main__':
     config.Data.inputDataset = '/ZZTo4L_13TeV_powheg_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
     crabCommand('submit', config = config)
 
-    # config.General.requestName = 'DYntuple'+version+'ST_tW'
-    # config.Data.inputDataset = '/ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/MINIAODSIM'
-    # crabCommand('submit', config = config)
+    config.General.requestName = 'DYntuple'+version+'ST_tW'
+    config.Data.inputDataset = '/ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/MINIAODSIM'
+    crabCommand('submit', config = config)
 
-    # config.General.requestName = 'DYntuple'+version+'ST_tbarW'
-    # config.Data.inputDataset = '/ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/MINIAODSIM'
-    # crabCommand('submit', config = config)
+    config.General.requestName = 'DYntuple'+version+'ST_tbarW'
+    config.Data.inputDataset = '/ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/MINIAODSIM'
+    crabCommand('submit', config = config)
+
+    config.General.requestName = 'DYntuple'+version+'WJetsToLNu'
+    config.Data.inputDataset = '/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext2-v2/MINIAODSIM'
+    crabCommand('submit', config = config)
 
     # config.General.requestName = ''
     # config.Data.inputDataset = ''
