862c8acb8a40e9fd10490004a47b41197e228cfc
Dalmin Pai <dalmin.pai@cern.ch>
2018-02-09 21:58:55 +0900

    change to v2p3

--- DYntupleMaker/ntuples/CRABSubmit/crab3cfg_MC_WJetsToLNu_Moriond17.py
+++ DYntupleMaker/ntuples/CRABSubmit/crab3cfg_MC_WJetsToLNu_Moriond17.py
@@ -4,7 +4,7 @@ from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
 #version = '_v2p0_'
-version = '_v2p2_'
+version = '_v2p3_'
 
 config.General.requestName = ''
 config.General.workArea = 'DYntuple'
